20476785c41d46d99213554b2123940a936656a9
matheusnw1 <134068420+matheusnw1@users.noreply.github.com>
2026-06-08 23:26:27 -0300

    Implement age survey program in C

--- idade.c
+++ idade.c
@@ -1,1 +1,30 @@
 
+#include <stdio.h>
+
+int main() {
+    int idade[15];
+    int maiores = 0, menores = 0;
+    float soma = 0, media;
+
+    printf("=== Pesquisa de Idades ===\n");
+
+    for(int i = 0; i < 15; i++) {
+        printf("Digite a idade da pessoa %d: ", i + 1);
+        scanf("%d", &idade[i]);
+
+        soma += idade[i];
+
+        if(idade[i] >= 18)
+            maiores++;
+        else
+            menores++;
+    }
+
+    media = soma / 15;
+
+    printf("\nMedia de idade: %.1f\n", media);
+    printf("Maiores de idade: %d\n", maiores);
+    printf("Menores de idade: %d\n", menores);
+
+    return 0;
+}
