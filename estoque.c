66d8a98907dcc982f14cadcc8ed45bb70a4574d1
matheusnw1 <134068420+matheusnw1@users.noreply.github.com>
2026-06-11 23:47:50 -0300

    Update estoque.c

--- estoque.c
+++ estoque.c
@@ -1,1 +1,28 @@
 
+#include <stdio.h>
+
+int main() {
+    int estoque[10];
+    int total = 0, maior, menor;
+
+    printf("=== Controle de Estoque ===\n");
+
+    for(int i = 0; i < 10; i++) {
+        printf("Quantidade do produto %d: ", i + 1);
+        scanf("%d", &estoque[i]);
+        total += estoque[i];
+    }
+
+    maior = menor = estoque[0];
+
+    for(int i = 0; i < 10; i++) {
+        if(estoque[i] > maior) maior = estoque[i];
+        if(estoque[i] < menor) menor = estoque[i];
+    }
+
+    printf("\nTotal de itens: %d\n", total);
+    printf("Maior estoque: %d\n", maior);
+    printf("Menor estoque: %d\n", menor);
+
+    return 0;
+}
