71c4a6c84d29d289a77a50b272685bcc40f82fa4
matheusnw1 <134068420+matheusnw1@users.noreply.github.com>
2026-06-16 18:43:49 -0300

    Update paridade.c

--- paridade.c
+++ paridade.c
@@ -1,1 +1,28 @@
 
+#include <stdio.h>
+
+int main() {
+    int v[10], pares[10], impares[10];
+    int i, np = 0, ni = 0;
+
+    for (i = 0; i < 10; i++) {
+        printf("Numero %d: ", i + 1);
+        scanf("%d", &v[i]);
+        if (v[i] % 2 == 0)
+            pares[np++] = v[i];
+        else
+            impares[ni++] = v[i];
+    }
+
+    printf("\nOriginal: ");
+    for (i = 0; i < 10; i++) printf("%d ", v[i]);
+
+    printf("\nPares: ");
+    for (i = 0; i < np; i++) printf("%d ", pares[i]);
+
+    printf("\nImpares: ");
+    for (i = 0; i < ni; i++) printf("%d ", impares[i]);
+
+    printf("\n");
+    return 0;
+}
