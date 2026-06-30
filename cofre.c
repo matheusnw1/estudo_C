1ef0e06a6ce6561f39dd96ce823e8a1f8f2d353c
matheusnw1 <134068420+matheusnw1@users.noreply.github.com>
2026-06-29 22:46:44 -0300

    Update cofre.c

--- cofre.c
+++ cofre.c
@@ -1,1 +1,29 @@
 
+#include <stdio.h>
+
+int main() {
+
+    int senha = 1234;
+    int tentativa;
+    int tentativas = 3;
+
+    while (tentativas > 0) {
+
+        printf("Digite a senha: ");
+        scanf("%d", &tentativa);
+
+        if (tentativa == senha) {
+            printf("\nAcesso permitido!\n");
+            return 0;
+        }
+
+        tentativas--;
+
+        if (tentativas > 0)
+            printf("Senha incorreta! Restam %d tentativa(s).\n\n", tentativas);
+    }
+
+    printf("\nConta bloqueada!\n");
+
+    return 0;
+}
