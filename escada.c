8f8c249802bc861538eb3cf69c888a1e5c76407b
matheusnw1 <134068420+matheusnw1@users.noreply.github.com>
2026-07-01 15:39:35 -0300

    Implement right triangle pattern printing
    Add a program to print a right triangle pattern based on user input.

--- escada.c
+++ escada.c
@@ -1,1 +1,23 @@
 
+#include <stdio.h>
+
+int main() {
+
+    int linhas;
+
+    printf("Digite um numero: ");
+    scanf("%d", &linhas);
+
+    printf("\n");
+
+    for(int i = 1; i <= linhas; i++) {
+
+        for(int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
