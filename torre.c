6771b7ce75aab41c41c8e412625b6d8ddf749cd9
matheusnw1 <134068420+matheusnw1@users.noreply.github.com>
2026-07-02 23:18:45 -0300

    Update torre.c

--- torre.c
+++ torre.c
@@ -1,1 +1,34 @@
 
+#include <stdio.h>
+
+int main() {
+
+    int linhas;
+
+    printf("Digite a quantidade de linhas: ");
+    scanf("%d", &linhas);
+
+    printf("\nTorre Crescente:\n");
+
+    for(int i = 1; i <= linhas; i++) {
+
+        for(int j = 1; j <= i; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    printf("\nTorre Decrescente:\n");
+
+    for(int i = linhas; i >= 1; i--) {
+
+        for(int j = 1; j <= i; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
