8c5cda50f1f1c6ccc1e0db0f48ba4c98021dc781
matheusnw1 <134068420+matheusnw1@users.noreply.github.com>
2026-07-03 23:10:41 -0300

    Update inverso.c

--- inverso.c
+++ inverso.c
@@ -1,1 +1,24 @@
 
+#include <stdio.h>
+
+int main() {
+
+    int numero;
+    int invertido = 0;
+    int resto;
+
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &numero);
+
+    while(numero > 0){
+
+        resto = numero % 10;
+        invertido = invertido * 10 + resto;
+        numero = numero / 10;
+
+    }
+
+    printf("Numero invertido: %d\n", invertido);
+
+    return 0;
+}
