70e092af9b432ea790d90760c25ddce4824df68c
matheusnw1 <134068420+matheusnw1@users.noreply.github.com>
2026-07-06 22:26:31 -0300

    Update chute.c

--- chute.c
+++ chute.c
@@ -1,1 +1,32 @@
 
+#include <stdio.h>
+
+int main() {
+
+    int segredo = 4321;
+    int chute;
+    int tentativas = 0;
+
+    printf("===== CAIXA FORTE =====\n");
+
+    do{
+
+        printf("Digite seu palpite: ");
+        scanf("%d", &chute);
+
+        tentativas++;
+
+        if(chute > segredo){
+            printf("O numero secreto e MENOR!\n\n");
+        }
+        else if(chute < segredo){
+            printf("O numero secreto e MAIOR!\n\n");
+        }
+
+    }while(chute != segredo);
+
+    printf("\nParabens! Voce acertou!\n");
+    printf("Tentativas: %d\n", tentativas);
+
+    return 0;
+}
