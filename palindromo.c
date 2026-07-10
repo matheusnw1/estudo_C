2f238d9ae80d67d52f48fc567652e9794917aa55
matheusnw1 <134068420+matheusnw1@users.noreply.github.com>
2026-07-10 20:25:39 -0300

    Update palindromo.c

--- palindromo.c
+++ palindromo.c
@@ -1,1 +1,30 @@
 
+#include <stdio.h>
+
+int main() {
+
+    int numero;
+    int copia;
+    int invertido = 0;
+    int resto;
+
+    printf("Digite um numero: ");
+    scanf("%d",&numero);
+
+    copia = numero;
+
+    while(copia > 0){
+
+        resto = copia % 10;
+        invertido = invertido * 10 + resto;
+        copia /= 10;
+
+    }
+
+    if(numero == invertido)
+        printf("O numero e um palindromo.");
+    else
+        printf("O numero nao e um palindromo.");
+
+    return 0;
+}
