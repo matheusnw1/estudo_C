4e896e41d4bf820e9cfef5bbebb8fcaa39667ad4
matheusnw1 <134068420+matheusnw1@users.noreply.github.com>
2026-07-11 23:57:23 -0300

    Update conversor.c

--- conversor.c
+++ conversor.c
@@ -1,1 +1,23 @@
 
+#include <stdio.h>
+
+int main() {
+
+    int segundos;
+    int horas, minutos, resto;
+
+    printf("Digite a quantidade de segundos: ");
+    scanf("%d", &segundos);
+
+    horas = segundos / 3600;
+    resto = segundos % 3600;
+
+    minutos = resto / 60;
+    resto = resto % 60;
+
+    printf("\n%d hora(s)\n", horas);
+    printf("%d minuto(s)\n", minutos);
+    printf("%d segundo(s)\n", resto);
+
+    return 0;
+}
