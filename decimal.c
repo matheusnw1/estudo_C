2c250f63d15580bf2d28b7fedd231d059157c485
matheusnw1 <134068420+matheusnw1@users.noreply.github.com>
2026-07-15 23:55:15 -0300

    Update decimal.c
    Add a program to convert decimal to binary.

--- decimal.c
+++ decimal.c
@@ -1,1 +1,33 @@
 
+#include <stdio.h>
+
+int main() {
+
+    int numero;
+    int binario[32];
+    int i = 0;
+
+    printf("Digite um numero decimal: ");
+    scanf("%d", &numero);
+
+    if(numero == 0){
+        printf("Binario: 0");
+        return 0;
+    }
+
+    while(numero > 0){
+
+        binario[i] = numero % 2;
+        numero = numero / 2;
+        i++;
+
+    }
+
+    printf("\nBinario: ");
+
+    for(int j = i - 1; j >= 0; j--){
+        printf("%d", binario[j]);
+    }
+
+    return 0;
+}
