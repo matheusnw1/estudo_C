cda2922c7a0977b56e5aea4926c2d63f207d54f1
matheusnw1 <134068420+matheusnw1@users.noreply.github.com>
2026-08-15 23:47:36 -0300

    Update pi.c

--- pi.c
+++ pi.c
@@ -1,1 +1,34 @@
 
+#include <stdio.h>
+
+int main() {
+
+    long long termos;
+    double pi = 0.0;
+    double sinal = 1.0;
+
+    printf("===== CALCULO DE PI =====\n");
+
+    printf("Digite a quantidade de termos: ");
+    scanf("%lld", &termos);
+
+    if(termos <= 0) {
+        printf("Quantidade invalida!\n");
+        return 0;
+    }
+
+    for(long long i = 0; i < termos; i++) {
+
+        pi += sinal / (2 * i + 1);
+
+        sinal = -sinal;
+    }
+
+    pi *= 4;
+
+    printf("\nQuantidade de termos: %lld\n", termos);
+    printf("Valor aproximado de PI: %.15f\n", pi);
+    printf("Valor real aproximado: 3.141592653589793\n");
+
+    return 0;
+}
